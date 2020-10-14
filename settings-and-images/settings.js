4f58d2d8ced5e658deb3de8b4c404f29ed87a23e
Draconis <olivier@delort.email>
2020-10-14 11:38:18 +0200

    première livraison

---
+++ settings-and-images/settings.js
@@ -0,0 +1,18 @@
+var settings = {
+    "options": {
+        "pauseTime": "6",
+        "inbetweenPauseTime": "100"
+    },
+    "colors": {
+        "iconBoxColor": "rgba(249,158,26)",
+        "textBoxColor": "rgba(64,82,117)",
+        "iconColor": "rgba(64,82,117)",
+        "fontColor": "rgba(255,255,255)"
+    },
+    "fonts": {
+        "primaryFont": "Inconsolata",
+        "fontWeight": "400",
+        "fontSize": "36",
+        "textYOffset": "0"
+    }
+};
